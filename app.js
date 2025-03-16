076da0752827218229ff1425ce57ec9c5981defc
madhul91 <madhuldubey06@gmail.com>
2025-03-16 23:07:38 +0530

    vc

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ config({ path: "./config.env" });
 
 app.use(
   cors({
-    origin: "https://frontend-gym-blue.vercel.app",
+    origin: "https://frontend-gym-blue.vercel.app/send/email",
     methods: ["POST"],
     credentials: true,
   })
